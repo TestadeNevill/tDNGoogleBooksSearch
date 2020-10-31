c783da3b799e68e43f9474863a6221a8faec19bc
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-10-31 03:27:04 -0400

    removed old css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/search";
